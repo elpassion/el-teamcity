1319d5e719c8bbc46c10a32e1eb99276935ef44a
Marek Langiewicz <marek.langiewicz@gmail.com>
2017-06-30 09:50:29 +0200

    [refactor] Extract subscribing and printing in tests

--- app/src/test/java/pl/elpassion/eltc/TeamCityRealApiTest.kt
+++ app/src/test/java/pl/elpassion/eltc/TeamCityRealApiTest.kt
@@ -2,6 +2,7 @@
 
 package pl.elpassion.eltc
 
+import io.reactivex.Single
 import org.junit.Ignore
 import org.junit.Test
 
@@ -12,31 +13,24 @@ class TeamCityRealApiTest {
 
     @Test
     fun `Get builds after call to real TeamCity API`() {
-        TeamCityApiImpl.getBuilds(CREDENTIALS)
-                .subscribe({
-                    println(it.joinToString { "\n$it" })
-                }, {
-                    throw it
-                })
+        TeamCityApiImpl
+                .getBuilds(CREDENTIALS)
+                .getAndPrint()
     }
 
     @Test
     fun `Get single build after call to real TeamCity API`() {
-        TeamCityApiImpl.getBuild(CREDENTIALS, id = 1)
-                .subscribe({
-                    println(it)
-                }, {
-                    throw it
-                })
+        TeamCityApiImpl
+                .getBuild(CREDENTIALS, id = 1)
+                .getAndPrint()
     }
 
     @Test
     fun `Get all tests per build after call to real TeamCity API`() {
-        TeamCityApiImpl.getTests(CREDENTIALS, buildId = 668)
-                .subscribe({
-                    println(it)
-                }, {
-                    throw it
-                })
+        TeamCityApiImpl
+                .getTests(CREDENTIALS, buildId = 668)
+                .getAndPrint()
     }
+
+    fun <T> Single<T>.getAndPrint() = println(blockingGet())
 }
