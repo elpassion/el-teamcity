19744caf7308415d74c227caf809c835c379c772
Paweł Gajda <pawegio@gmail.com>
2017-07-07 14:21:37 +0200

    [refactoring] Make lifecycle registry private

--- app/src/main/java/pl/elpassion/eltc/ui/BaseActivity.kt
+++ app/src/main/java/pl/elpassion/eltc/ui/BaseActivity.kt
@@ -5,6 +5,6 @@ import android.arch.lifecycle.LifecycleRegistryOwner
 import android.support.v7.app.AppCompatActivity
 
 open class BaseActivity : AppCompatActivity(), LifecycleRegistryOwner {
-    val registry = LifecycleRegistry(this)
+    private val registry = LifecycleRegistry(this)
     override fun getLifecycle() = registry
 }
