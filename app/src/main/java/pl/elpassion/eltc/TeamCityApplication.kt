8b856a1d227ec1ff9107257b8395afc751bf99b8
Paweł Gajda <pawegio@gmail.com>
2017-07-18 16:31:22 +0200

    Handle UndeliverableException

--- app/src/main/java/pl/elpassion/eltc/TeamCityApplication.kt
+++ app/src/main/java/pl/elpassion/eltc/TeamCityApplication.kt
@@ -1,11 +1,25 @@
 package pl.elpassion.eltc
 
 import android.app.Application
+import android.util.Log
+import io.reactivex.exceptions.UndeliverableException
+import io.reactivex.plugins.RxJavaPlugins
 
 
 class TeamCityApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         DI.provideApplication = { this }
+    }
+
+    companion object {
+        init {
+            RxJavaPlugins.setErrorHandler { throwable ->
+                if (throwable is UndeliverableException) {
+                    Log.d("RxJavaPlugins", "error handler got: $throwable")
+                }
+                else throw throwable
+            }
+        }
     }
 }
