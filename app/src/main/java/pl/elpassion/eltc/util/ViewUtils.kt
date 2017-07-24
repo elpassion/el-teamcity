eb5ff136d3ed315de648c30fc96c33f0397f97aa
Paweł Gajda <pawegio@gmail.com>
2017-07-24 16:23:41 +0200

    [refactoring] Remove unused method

--- app/src/main/java/pl/elpassion/eltc/util/ViewUtils.kt
+++ app/src/main/java/pl/elpassion/eltc/util/ViewUtils.kt
@@ -2,9 +2,6 @@ package pl.elpassion.eltc.util
 
 import android.support.design.widget.Snackbar
 import android.view.View
-import android.view.ViewGroup
-
-val ViewGroup.views get() = (0 until childCount).map { getChildAt(it) }
 
 fun View.snack(message: String, length: Int = Snackbar.LENGTH_LONG) {
     val snack = Snackbar.make(this, message, length)
