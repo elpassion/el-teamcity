5c8389e1fc095f71eebf1d16a71499306abfc508
Paweł Gajda <pawegio@gmail.com>
2017-07-24 16:30:30 +0200

    [refactoring] Extract RxJava error handler setup

--- app/src/main/java/pl/elpassion/eltc/TeamCityApplication.kt
+++ app/src/main/java/pl/elpassion/eltc/TeamCityApplication.kt
@@ -1,12 +1,10 @@
 package pl.elpassion.eltc
 
 import android.app.Application
-import android.util.Log
-import io.reactivex.exceptions.UndeliverableException
-import io.reactivex.plugins.RxJavaPlugins
+import pl.elpassion.eltc.util.setupRxJavaErrorHandler
 
 
-class TeamCityApplication: Application() {
+class TeamCityApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         DI.provideApplication = { this }
@@ -14,12 +12,7 @@ class TeamCityApplication: Application() {
 
     companion object {
         init {
-            RxJavaPlugins.setErrorHandler { throwable ->
-                if (throwable is UndeliverableException) {
-                    Log.d("RxJavaPlugins", "error handler got: $throwable")
-                }
-                else throw throwable
-            }
+            setupRxJavaErrorHandler()
         }
     }
 }
--- app/src/main/java/pl/elpassion/eltc/util/RxUtils.kt
+++ app/src/main/java/pl/elpassion/eltc/util/RxUtils.kt
@@ -1,6 +1,17 @@
 package pl.elpassion.eltc.util
 
+import android.util.Log
 import io.reactivex.Single
+import io.reactivex.exceptions.UndeliverableException
+import io.reactivex.plugins.RxJavaPlugins
+
+fun setupRxJavaErrorHandler() {
+    RxJavaPlugins.setErrorHandler { throwable ->
+        if (throwable is UndeliverableException) {
+            Log.d("RxJavaPlugins", "error handler got: $throwable")
+        } else throw throwable
+    }
+}
 
 @Suppress("UNCHECKED_CAST")
 fun <T, R : Comparable<R>> zipSingles(singles: List<Single<List<T>>>, sortDescBy: (T) -> R): Single<List<T>> =
