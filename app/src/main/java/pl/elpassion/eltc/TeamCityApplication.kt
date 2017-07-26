dfcbf4c77e6859ad401c32bef97d2876b5b249cf
Paweł Gajda <pawegio@gmail.com>
2017-07-26 12:39:27 +0200

    Replace progress bar in builds list

--- app/src/main/java/pl/elpassion/eltc/TeamCityApplication.kt
+++ app/src/main/java/pl/elpassion/eltc/TeamCityApplication.kt
@@ -3,7 +3,6 @@ package pl.elpassion.eltc
 import android.app.Application
 import pl.elpassion.eltc.util.setupRxJavaErrorHandler
 
-
 class TeamCityApplication : Application() {
     override fun onCreate() {
         super.onCreate()
