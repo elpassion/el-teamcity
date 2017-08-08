4a166f60afeaa3b88fdbc96d6b762d4de6e14ee1
Paweł Gajda <pawegio@gmail.com>
2017-08-08 12:59:50 +0200

    #25 Post notifications with recap to status bar

--- app/src/main/java/pl/elpassion/eltc/recap/RecapNotifierImpl.kt
+++ app/src/main/java/pl/elpassion/eltc/recap/RecapNotifierImpl.kt
@@ -1,14 +1,32 @@
 package pl.elpassion.eltc.recap
 
 import android.app.Application
-import android.widget.Toast
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.support.v4.app.NotificationCompat
 import pl.elpassion.eltc.Build
+import pl.elpassion.eltc.R
+import pl.elpassion.eltc.builds.BuildsActivity
 
 class RecapNotifierImpl(private val application: Application) : RecapNotifier {
 
     override fun showFailureNotification(failedBuilds: List<Build>) {
-        //TODO: post notifications in status bar
-        val message = "Failed ${failedBuilds.count()} builds!"
-        Toast.makeText(application, message, Toast.LENGTH_LONG).show()
+        notify("${failedBuilds.count()} new builds failed.")
+    }
+
+    private fun notify(message: String) {
+        val resultIntent = PendingIntent.getActivity(application,
+                0, Intent(application, BuildsActivity::class.java), 0)
+        val notification = NotificationCompat.Builder(application)
+                .setSmallIcon(R.drawable.ic_failure)
+                .setContentTitle(application.getString(R.string.teamcity_recap))
+                .setContentText(message)
+                .setContentIntent(resultIntent)
+                .build()
+        val notificationManager = application
+                .getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(1, notification)
     }
 }
