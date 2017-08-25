51a0870a606a96bfd586941bcdccfdc6d20090c9
Paweł Gajda <pawegio@gmail.com>
2017-08-25 16:02:33 +0200

    [refactoring] Remove unused method

--- app/src/main/java/pl/elpassion/eltc/util/ContextUtils.kt
+++ app/src/main/java/pl/elpassion/eltc/util/ContextUtils.kt
@@ -2,15 +2,6 @@ package pl.elpassion.eltc.util
 
 import android.app.NotificationManager
 import android.content.Context
-import android.support.annotation.LayoutRes
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
-inline fun Context.inflate(@LayoutRes layoutResId: Int,
-                           parent: ViewGroup? = null,
-                           attachToRoot: Boolean = false): View
-        = LayoutInflater.from(this).inflate(layoutResId, parent, attachToRoot)
 
 inline val Context.notificationManager
     get() = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
