35d212440eb3e9b77ea555c467d43e16ff5a8e10
Paweł Gajda <pawegio@gmail.com>
2017-08-25 10:07:46 +0200

    Fix formatting date in for Java pre-1.8

--- app/src/main/java/pl/elpassion/eltc/details/util/DateUtils.kt
+++ app/src/main/java/pl/elpassion/eltc/details/util/DateUtils.kt
@@ -3,6 +3,6 @@ package pl.elpassion.eltc.details.util
 import java.text.SimpleDateFormat
 import java.util.*
 
-fun Date.toTime(): String = SimpleDateFormat("d MMM YY HH:mm:ss", Locale.US).format(this)
+fun Date.toTime(): String = SimpleDateFormat("d MMM yy HH:mm:ss", Locale.US).format(this)
 
 fun Date.toTimeWithoutDate(): String = SimpleDateFormat("HH:mm:ss", Locale.US).format(this)
